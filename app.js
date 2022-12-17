b1de95ca3cfb978e1a3581c2e9fa860c6cd3875f
Flavien <livinien@hotmail.fr>
2022-12-17 14:04:06 +0100

    animations des inputs

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
 // ANIMATION INPUTS //
 
+const inputs = document.querySelectorAll(".input-field");
+
+inputs.forEach((inp) => {
+    inp.addEventListener("focus", () => {
+        inp.classList.add("active");
+    });
+
+    inp.addEventListener("blur", () => {
+        if(inp.value != "") return;
+        inp.classList.remove("active");
+    });
+});
